e5c58ac75cb01a361e1ede31c26e1838b9504885
daFinndus <finnluca555@gmail.com>
2024-06-12 08:57:49 +0200

    Changed radius

--- camera/my_camera.py
+++ camera/my_camera.py
@@ -107,8 +107,8 @@ class MyCamera:
         # Find contours in the red mask
         contours, _ = cv2.findContours(white_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-        min_radius = 100
-        max_radius = 130
+        min_radius = 80
+        max_radius = 200
 
         # Store cups with their positions in our list
         cups = [(int(x), int(y), int(radius)) for cnt in contours if cv2.contourArea(cnt) > 500 for ((x, y), radius) in
