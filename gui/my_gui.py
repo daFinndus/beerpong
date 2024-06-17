3b0ba688ff8f5c2a696e59da7e3024dfca5d3c1e
daFinndus <finnluca555@gmail.com>
2024-06-17 13:10:09 +0200

    Fixed something in GUI

--- gui/my_gui.py
+++ gui/my_gui.py
@@ -53,7 +53,10 @@ class MyGUI:
 
             if (x, y, radius) in self.hit_cups:
                 print(f"Cup at position: {x, y} with radius: {radius} is in the hit_cups list!")
-                self.root_counter = len(self.hit_cups)
+                if self.hit_cups:
+                    self.root_counter = len(self.hit_cups)
+                else:
+                    self.root_counter = 0
                 self.score_label.configure(text=f"Score: {self.root_counter}")
                 color = "gray"
             else:
